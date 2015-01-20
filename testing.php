cd723a9befdf71d51fcd52a2d030ae7dc496248c
[Nagaraj] <nagarajbgk@gmail.com>
2015-01-20 18:40:39 +0530

    commited

--- testing.php
+++ testing.php
@@ -4,7 +4,6 @@ require_once("config.php");
 require_once("$CFG->dirroot/user/profile/lib.php");
 
 require_login();
-
 /*$newuserobj = new stdClass();
 $newuserobj->profile_field_RNO="123rfgg";
 $newuserobj->id = $USER->id;
@@ -12,14 +11,11 @@ profile_save_data($newuserobj);
 $user = $DB->get_record('user', array('id'=>$newuserobj->id));
 $table='user'; 
 */
-
 $user=$DB->get_record_sql('SELECT * FROM {user} WHERE firstname = :firstname AND lastname = :lastname', 
                        array('firstname' => 'Nagaraj','lastname'=> 'K'));
 $conditions='';
 $table='user'; 
-
 //$user=$DB->get_record($table, array('id'=>$newuserobj->id) , $fields='*', $strictness=IGNORE_MISSING); 
-
 echo "<pre>";
 print_r($user);
 echo "</pre>";
