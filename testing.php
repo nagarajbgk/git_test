2a442d7a5a2290bf7e6064209c9cf36e6555b82f
[Nagaraj] <nagarajbgk@gmail.com>
2015-01-21 11:44:50 +0530

    new commit

--- testing.php
+++ testing.php
@@ -12,13 +12,6 @@ $user = $DB->get_record('user', array('id'=>$newuserobj->id));
 $table='user'; 
 */
 
-
-
-
-
-
-
-
 $user=$DB->get_record_sql('SELECT * FROM {user} WHERE firstname = :firstname AND lastname = :lastname', 
                        array('firstname' => 'Nagaraj','lastname'=> 'K'));
 $conditions='';
